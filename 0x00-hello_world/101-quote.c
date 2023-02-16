3def904f366e3754c32be8647d2607b414dc5591
Stankovic5412 <leonardobasi61@gmail.com>
2023-02-16 14:21:11 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Return : 1
- */
+ * Return : 1 (Success)
+*/
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
