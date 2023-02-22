3d3a5c251f5b0204657275ace25f427a1cf6d823
Stankovic5412 <leonardobasi61@gmail.com>
2023-02-21 20:17:33 -0800

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table -prints the n times table, starting with 0
- *
+ * @n: number of the times table
  */
 
 void print_times_table(int n)
@@ -29,7 +29,6 @@ void print_times_table(int n)
 				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
